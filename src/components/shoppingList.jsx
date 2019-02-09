8340118f9563953799edc288edaf0dedf8e0c2c0
Kyle White <kwhite8933@gmail.com>
2019-02-09 12:50:25 -0500

    update dev branch

--- src/components/shoppingList.jsx
+++ src/components/shoppingList.jsx
@@ -12,48 +12,86 @@ class ShoppingList extends React.Component {
                 {name: "American Cheese", status: false, hide: false},
                 {name: "Panini Bread", status: false, hide: false}
             ],
-            hidden: [],
-            hideList: false
+            itemsHidden: true,
+            hideListButton: true,
         }
     }
 
-    // Add checked off items to archived list to be viewed later 
-
+    // Change the status of each item
     changeItemStatus = (name, status, hidden) => {
         let _list = this.state.list;
         
         for(let i=0; i<_list.length; i++){
             if(_list[i].name === name){
-                _list[i].status = status;
-                _list[i].hide = hidden;
+                if(_list[i].status === true && hidden === true){
+                    _list[i].status = status;
+                }
+                else{
+                    _list[i].status = status;
+                    _list[i].hide = hidden;
+                }
             }
         }
 
-        this.setState({
-            list: _list
-        });
+        let _itemsHidden = false;
+        for(let i=0; i<_list.length; i++){
+            if(_list[i].hide === true){
+                _itemsHidden = true;
+            }
+        }
+
+        if(this.state.hideListButton){
+            this.setState({
+                list: _list,
+                itemsHidden: _itemsHidden,
+                hideListButton: false
+            })
+        }
+        else
+        {
+            this.setState({
+                list: _list,
+                itemsHidden: _itemsHidden
+            })
+        }
+        
     }
 
     // toggle hiding and showing items when they are checked
     toggleCheckedItems = () => {
         let _list = this.state.list;
 
+        // for(let i=0; i<_list.length; i++){
+        //     if(this.state.hideList){
+        //         if(_list[i].status === true){
+        //             _list[i].hide = true;    
+        //         }
+        //         else if(_list[i].status === false){
+        //             _list[i].hide = false;
+        //         }
+        //     }
+        //     else{
+        //         if(_list[i].status === true){
+        //             _list[i].hide = false;
+        //         }
+        //         else if(_list[i].status === false){
+        //             _list[i].hide = false;
+        //         }
+        //     }
+        // }
         for(let i=0; i<_list.length; i++){
-            if(this.state.hideList){
-                if(_list[i].status === true){
-                    _list[i].hide = true;    
-                }
+            if(!this.state.itemsHidden){
+                _list[i].hide = _list[i].status ? true : false;
             }
-            else{
-                if(_list[i].status === true){
-                    _list[i].hide = false;
-                }
+            else
+            {
+                _list[i].hide = false;
             }
         }
 
         this.setState({
             list: _list,
-            hideList: !this.state.hideList
+            itemsHidden: !this.state.itemsHidden
         });
 
     }
@@ -70,7 +108,17 @@ class ShoppingList extends React.Component {
                         })
                     }
                 </form>
-                <p id="inCartItems" onClick={this.toggleCheckedItems}>Show All...</p>
+                {
+                    this.state.hideListButton
+                    ?
+                        <div></div>
+                    :
+                        this.state.itemsHidden
+                        ?
+                            <p id="toggleInCartItems" onClick={this.toggleCheckedItems}>Show Checked...</p>
+                        :
+                            <p id="toggleInCartItems" onClick={this.toggleCheckedItems}>Hide Checked...</p>
+                }
             </div>
         );
     }
